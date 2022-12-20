631b2a0d83c63bdb09489030e3f08f908e4f285c
Arturo Pérez <77728050+ARTP3R@users.noreply.github.com>
2022-12-20 22:31:40 +0100

    Create extract_ips_v6.py

---
+++ extract_ips_v6.py
@@ -0,0 +1,22 @@
+import re
+
+# Abrimos el archivo
+with open('ips.txt', 'r') as f:
+  # Leemos el contenido del archivo
+  content = f.read()
+
+# Creamos una regex para coincidir con direcciones IP
+ip_regex = r'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
+
+# Utilizamos la regex para extraer todas las direcciones IP del contenido del archivo
+ips = re.findall(ip_regex, content)
+
+# Creamos un conjunto para almacenar las direcciones IP únicas
+unique_ips = set()
+
+# Iteramos sobre las direcciones IP extraídas
+for ip in ips:
+  # Si la dirección IP no está en el conjunto, la añadimos y la mostramos por pantalla
+  if ip not in unique_ips:
+    unique_ips.add(ip)
+    print(ip)
