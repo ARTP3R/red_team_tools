a78579bdcc0a945e81104070d0fd0765bd604b49
Arturo Pérez <77728050+ARTP3R@users.noreply.github.com>
2024-02-27 16:43:57 +0100

    Update call_trans_opt.sh
    Delay setup

--- call_trans_opt.sh
+++ call_trans_opt.sh
@@ -4,7 +4,8 @@
 # Add next function and call to .bashrc or .zshrc 
 
 enter_matrix(){
-	local matrix_text_delay=0.025
+	local matrix_text_delay=0.025 # Movie 0.025
+	local matrix_delay=0.5 # Movie 1
 	local matrix_green_text="$(tput bold)\e[32m"
 	local matrix_reset_text="\e[0m$(tput sgr0)"
 	local matrix_date=$(date +'%d-%m-%y')
@@ -13,21 +14,21 @@ enter_matrix(){
 	local matrix_line2="Trace program: running"
 	local matrix_line3="${(%%)PROMPT}"
 	clear
-	sleep 3
+	sleep $((3*$matrix_delay))
 	for ((i = 0; i < ${#matrix_line1}; i++)); do
 	    echo -en "${matrix_green_text}${matrix_line1:$i:1}${matrix_reset_text}"
 	    sleep $matrix_text_delay
 	done
-	sleep 3
+	sleep $((3*$matrix_delay))
 	clear
-	sleep 1
+	sleep $((1*$matrix_delay))
 	for ((i = 0; i < ${#matrix_line2}; i++)); do
 	    echo -en "${matrix_green_text}${matrix_line2:$i:1}${matrix_reset_text}"
 	    sleep $matrix_text_delay
 	done
-	sleep 3
+	sleep $((3*$matrix_delay))
 	clear
-	sleep 1
+	sleep $((1*$matrix_delay))
 	for ((i = 0; i < ${#matrix_line3}; i++)); do
 	    echo -en "${matrix_line3:$i:1}"
 	    sleep $matrix_text_delay
